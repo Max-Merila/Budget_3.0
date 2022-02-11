680061a96cf695c965ed31144c43823e3083abe5
Max Merila <90065604+Max-Merila@users.noreply.github.com>
2022-02-10 23:44:21 -0600

    Add files via upload
    added source tag to html, variables in js and a function to update the set budget.

--- Budget_3.0/budget.js
+++ Budget_3.0/budget.js
@@ -1,1 +1,15 @@
-var = budget
+// Variables
+const setBudget = document.getElementById("setBudget");
+const budgetSave = document.getElementById("budgetSave");
+const budgetOut = document.getElementById("budgetOut");
+const expensesOut = document.getElementById("expensesOut");
+const remainderOut = document.getElementById("remainderOut");
+const setExpDescription = document.getElementById("setExpDescription");
+const setExpAmount = document.getElementById("setExpAmount");
+const displayOut = document.getElementById("displayOut");
+// end Variables
+
+budgetSave.onclick = function() {
+    const Budget = setBudget.value;
+    budgetOut.innerHTML = Budget;
+}
