1d2572fd921d682724ea398b423ed38936f0a0cd
Max Merila <90065604+Max-Merila@users.noreply.github.com>
2022-02-15 15:45:01 -0600

    Add files via upload
    Added "add expense" button started expenseSave function

--- Budget_3.0/budget.js
+++ Budget_3.0/budget.js
@@ -7,9 +7,14 @@ const remainderOut = document.getElementById("remainderOut");
 const setExpDescription = document.getElementById("setExpDescription");
 const setExpAmount = document.getElementById("setExpAmount");
 const displayOut = document.getElementById("displayOut");
+let expOut = "";   
 // end Variables
 
 budgetSave.onclick = function() {
     const Budget = setBudget.value;
     budgetOut.innerHTML = Budget;
+}
+
+expenseSave.onclick = function() {
+    
 }
